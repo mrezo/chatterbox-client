b1c49e326b9933dc33c2df1367fcd5cfaeeed5c1
mrezo <balarezo.m@gmail.com>
2016-03-14 15:30:53 -0700

    Add fetch method to the app object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -25,6 +25,21 @@ app.send = function() {
   });
 };
 
+app.fetch = function () {
+  $.ajax({
+    url: undefined,
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message', data);
+    }
+  });
+};
 // $.ajax({
 //   // This is the url you should use to communicate with the parse API server.
 //   url: 'https://api.parse.com/1/classes/messages',
@@ -40,17 +55,4 @@ app.send = function() {
 //   }
 // });
 
-// $.ajax({
-//   url: 'https://api.parse.com/1/classes/messages',
-//   type: 'GET',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent');
-//   },
-//   error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message', data);
-   
-// });
 
