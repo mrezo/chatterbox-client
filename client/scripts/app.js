909f39d1c30794c21c7ebc1697de0726f977d34c
mrezo <balarezo.m@gmail.com>
2016-03-15 11:49:48 -0700

    Submit a message from the form field with a username the user selects

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,11 @@
-// YOUR CODE HERE:
 var app = {};
-var message = {
-  username: 'SUPER GREY ANIMAL',
-  text: 'OHHHH MA GAAAAAA',
-  roomname: 'aquarium'
-};
 
 app.init = function() {
   $(document).on('click', '.username', app.addFriend);
   $(document).on('click', '.submit', app.handleSubmit);
 };
 
-app.send = function() {
+app.send = function(message) {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
@@ -35,10 +29,17 @@ app.fetch = function () {
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
-      app.clearMessages();
+      app.clearAll();
+      var roomNames = {};
       for (var i = 0; i < data.results.length; i++) {
         if (data.results[i].username) {
-          app.addMessage(_stringFilter(data.results[i]));
+          var temp = _stringFilter(data.results[i]);
+          app.addMessage(temp);
+
+          if (!roomNames[temp.roomname]) {
+            $('.select').append('<option>' + temp.roomname + '</option>');
+            roomNames[temp.roomname] = temp.roomname;
+          }
         }
       }
     },
@@ -51,38 +52,50 @@ app.fetch = function () {
 
 var _stringFilter = function(object) {
   var temp = object;
-  var tempUsername = temp.username.split('');
-  var tempText = temp.text.split(''); 
-  var tempRoom = temp.roomname.split('');
-  for (var i = 0; i < tempUsername.length; i++) {
-    if (tempUsername[i] === '<' || tempUsername[i] === '(') {
-      tempUsername[i] = ' ';
+
+  if (temp.username) {    
+    var tempUsername = temp.username.split('');
+    for (var i = 0; i < tempUsername.length; i++) {
+      if (tempUsername[i] === '<' || tempUsername[i] === '(') {
+        tempUsername[i] = '_';
+      }
     }
+    temp.username = tempUsername.join('');
+  } else {
+    temp.username = 'anonymous';
   }
 
-  temp.username = tempUsername.join('');
-
-  for (var i = 0; i < tempText.length; i++) {
-    if (tempText[i] === '<' || tempText[i] === '(') {
-      tempText[i] = ' ';
+  if (temp.text) {
+    var tempText = temp.text.split(''); 
+    for (var i = 0; i < tempText.length; i++) {
+      if (tempText[i] === '<' || tempText[i] === '(') {
+        tempText[i] = '_';
+      }
     }
+    temp.text = tempText.join('');    
+  } else {
+    temp.text = '';
   }
 
-  temp.text = tempText.join('');
-
-  for (var i = 0; i < tempRoom.length; i++) {
-    if (tempRoom[i] === '<' || tempRoom[i] === '(') {
-      tempRoom[i] = ' ';
+  if (temp.roomname) {
+    var tempRoom = temp.roomname.split('');
+    for (var i = 0; i < tempRoom.length; i++) {
+      if (tempRoom[i] === '<' || tempRoom[i] === '(') {
+        tempRoom[i] = '_';
+      }
     }
+    temp.roomname = tempRoom.join('');    
+  } else {
+    temp.roomname = 'empty room';
   }
 
-  temp.roomname = tempRoom.join('');
 
   return temp;
 };
 
-app.clearMessages = function() {
+app.clearAll = function() {
   $('#chats').empty();
+  $('.select').empty();
 };
 
 app.addMessage = function (message) {
@@ -98,7 +111,34 @@ app.addFriend = function() {
 };
 
 app.handleSubmit = function() {
-  app.send();
+  var message = {
+    username: decodeURIComponent(document.location.search.split('=')[1]),
+    text: $('input').val(),
+    roomname: 'THE BEST ROOM EVA'
+  };
+  app.send(message);
+};
+
+app.user = function () {
+  $.ajax({
+    url: 'https://api.parse.com/1/users/ofNyMWNPHD',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+      app.clearMessages();
+      for (var i = 0; i < data.results.length; i++) {
+        if (data.results[i].username) {
+          app.addMessage(_stringFilter(data.results[i]));
+        }
+      }
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message', data);
+    }
+  });
 };
 
 setInterval(app.fetch, 500);
